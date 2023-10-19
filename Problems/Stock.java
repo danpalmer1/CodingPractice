c9d03abc5575fe901e42ba0b89151c0db3a3d6b9
danpalmer1 <xdanielpalmerx@gmail.com>
2023-10-18 21:28:49 -0500

    added setup for tomorrows problem

---
+++ Problems/Stock.java
@@ -0,0 +1,17 @@
+package Problems;
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+// Find and return the maximum profit you can achieve.
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
+
+// Input: prices = [1,2,3,4,5]
+// Output: 4
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+
+public class Stock {
+    
+}
